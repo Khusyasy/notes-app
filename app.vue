7108128cf31ea4a01750898832b7ad8a9e4c8102
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2024-01-26 06:51:37 +0700

    chore: install nuxt ui

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <UContainer>
+    <UButton @click="showModal = true">Show Modal</UButton>
+    <UModal v-model="showModal" title="Modal">
+      <UCard>
+        <p>Modal Content</p>
+      </UCard>
+    </UModal>
+  </UContainer>
 </template>
+
+<script setup lang="ts">
+const showModal = ref(false)
+</script>
